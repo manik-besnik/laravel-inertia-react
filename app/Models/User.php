e9da985e0d003e1239bc8724f47fa6d7a126bafb
manik-besnik <manikmia.dev@gmail.com>
2025-01-15 23:29:56 +0600

    Add Required  fields to users table

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,13 +3,20 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+
+/**
+ * @property string|null $provider_id
+ * @property string $name
+ * @property string $username
+ */
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
+    /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
 
     /**
@@ -18,8 +25,10 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
+        'provider_id',
         'name',
         'email',
+        'username',
         'password',
     ];
 
